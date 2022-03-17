5afc4d3d46bf9b5ea4a3ce5befcdfa582ea34101
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-03-17 12:48:21 +0530

    done

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+function navbar(){
+    return` <div id="nav">
+
+    <div id="main">
+         <h3><a href="home.html">Home</a></h3>
+     <h3><a href="receipe.html">Receipe of the day</a></h3>
+     <h3><a href="latest.html">Latest receipe</a></h3>
+ </div>
+    <div id="sign">
+       <h4><a href="login.html">Login</a></h4>
+       <h4><a href="signup.html">Signup</a></h4>
+    </div>
+ </div>`
+}
+export default navbar
