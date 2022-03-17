c5892a57ed0bb084475818962ea0109dde32debc
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-03-17 13:08:06 +0530

    done

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ function navbar(){
     return` <div id="nav">
 
     <div id="main">
-         <h3><a href="home.html">Home</a></h3>
+         <h3><a href="index.html">Home</a></h3>
      <h3><a href="receipe.html">Receipe of the day</a></h3>
      <h3><a href="latest.html">Latest receipe</a></h3>
  </div>
